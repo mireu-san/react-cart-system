fbdb578a77634f2165d3f901ee4f6ceb58ea17b1
Chesed Kim <starmireu@gmail.com>
2022-12-12 19:49:45 +0900

    shop done, add createContext

--- src/pages/shop/Goods.js
+++ src/pages/shop/Goods.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Goods.css";
 
 export const Goods = (props) => {
-  const { id, name, price, image } = props.shopItemProps;
-  // return <div>Goods{props.id}</div>;
+  const { id, name, price, image, link } = props.shopItemProps;
+  const [count, setCount] = useState(0);
   return (
     <>
       <div className="goods">
         <div>{id}</div>
         <img src={image} alt="thumbnail_image" />
-        <div>{name}</div>
-        <div>{price}</div>
+        <div className="goods-name">{name}</div>
+        <div className="goods-price">${price}</div>
+        <button>
+          <a href={link}>Official Store Page</a>
+        </button>
+        <div className="cart-button">
+          <button onClick={() => setCount(count - 1)}>-</button>
+          {count}
+          <button onClick={() => setCount(count + 1)}>+</button>
+        </div>
       </div>
     </>
   );
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -4,24 +4,16 @@ import { Goods } from "./Goods";
 import "./Shop.css";
 
 export const Shop = () => {
-  // render() {
-  // const shopItem = shopItems[0];
   return (
     <div className="shop">
-      <div className="shop-title">Shop Title</div>
+      <div className="shop-title">
+        <h1>Anime Biased Shop</h1>
+      </div>
       <div className="items">
         {ItemsList.map((item) => (
-          // data is props which I named by myself
           <Goods shopItemProps={item} />
         ))}
       </div>
-      {/* <Item
-        id={shopItem.id}
-        name={shopItem.name}
-        price={shopItem.price}
-        image={shopItem.image}
-      /> */}
     </div>
   );
 };
-// }
