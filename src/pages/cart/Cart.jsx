33e7c7540fe64160c18175bba1529b7decdea9d8
Chesed Kim <starmireu@gmail.com>
2022-12-13 17:28:01 +0900

    follows the concept - cart.jsx

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -26,7 +26,7 @@ export const Cart = () => {
       </div>
       {totalAmount > 0 ? (
         <div className="checkout">
-          <p> Subtotal: ${totalAmount}</p>
+          <p> Estimated Expenditure (예상 지출 금액): ${totalAmount}</p>
           <button onClick={() => navigate("/")}> Explore Other Items </button>
           <h3>To remove, decrement the quantity of your item to ZERO.</h3>
           <h3>목록에서 아이템을 지우려면 수량을 0으로 입력하세요.</h3>
