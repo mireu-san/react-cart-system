de98743aae55d824abeaea5d36981ff141b9eedd
Chesed Kim <starmireu@gmail.com>
2022-12-15 17:44:47 +0900

    minor fix - capital letter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navbar } from "./components/navbar/Navbar";
+import { Navbar } from "./components/Navbar/Navbar";
 import { Shop } from "./pages/shop/Shop";
 import { Cart } from "./pages/cart/Cart";
 import { DataContextProvider } from "./components/context/DataContext";
